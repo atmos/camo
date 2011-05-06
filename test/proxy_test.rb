90d0e3fc19a0e29413154fd3eea387cf10bfffa8
Corey Donohoe <atmos@atmos.org>
2011-05-06 12:10:25 -0700

    showoff bad regex test

--- test/proxy_test.rb
+++ test/proxy_test.rb
@@ -18,6 +18,11 @@ module CamoProxyTests
     assert_equal(200, response.code)
   end
 
+  def test_proxy_valid_image_url_with_crazy_subdomain
+    response = request('http://27.media.tumblr.com/tumblr_lkp6rdDfRi1qce6mto1_500.jpg')
+    assert_equal(200, response.code)
+  end
+
   def test_proxy_valid_google_chart_url
     response = request('http://chart.apis.google.com/chart?chs=920x200&chxl=0:%7C2010-08-13%7C2010-09-12%7C2010-10-12%7C2010-11-11%7C1:%7C0%7C0%7C0%7C0%7C0%7C0&chm=B,EBF5FB,0,0,0&chco=008Cd6&chls=3,1,0&chg=8.3,20,1,4&chd=s:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA&chxt=x,y&cht=lc')
     assert_equal(200, response.code)
